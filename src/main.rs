8cfdb2236163a8f1d284954f1599626250e5dd48
bpzj <1521189886@qq.com>
2020-12-10 20:33:31 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use std::io;
 
-fn main() {
-    println!("猜数字, 请输入你的猜测：");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("读取失败");
-    println!("你猜的数字是：{}", guess);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::blocking::get("https://httpbin.org/ip")?.text()?;
+    println!("{:#?}", resp);
+    Ok(())
 }
